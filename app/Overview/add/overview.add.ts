3bdf492e1e1b54d227d55b9804e09597189dd3db
nareshdevabatini <naresh.devabatini@gmail.com>
2017-05-24 13:08:23 +0530

    Add files via upload

---
+++ app/Overview/add/overview.add.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from "@angular/core"
+import { Person } from '../../models/PersonModel';
+
+
+@Component({
+    templateUrl: './app/Overview/add/overview.add.tpl.html'
+})
+
+export class AppOverviewAdd {
+    
+    public person: Person=new Person();
+    
+
+    
+    
+}
