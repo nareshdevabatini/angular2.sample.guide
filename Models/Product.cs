c1b7acaba5c15faf96ba4616ae0327bdbdea548f
nareshdevabatini <naresh.devabatini@gmail.com>
2017-05-24 14:33:10 +0530

    Create Product.cs

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Angular2.Sample.Forms.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
