1686ed925d05e215a4d1fcfd79a723e1c7794fab
nareshdevabatini <naresh.devabatini@gmail.com>
2017-05-24 14:33:53 +0530

    Create Roles.cs

---
+++ Models/Roles.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Angular2.Sample.Forms.Models
+{
+    public class Roles
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
